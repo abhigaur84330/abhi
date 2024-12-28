1a7dd1307086d495ebd2fd86bafed2bb1d5c0193
abhigaur84330 <gaur42973@gmail.com>
2024-12-28 14:31:30 +0530

    Create abhi.c

---
+++ abhi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main();
+{
+  printf("happy new year 2025\n");
+  printf(" to all my friend/n"):
+  printf("I wish you all to you enjoy this year with joy and happines /n"):
+    printf(" and say byy to 2024/n");
+  printf(" your friend/n");
+  printf("abhi/n");
+  return 0;
+}
